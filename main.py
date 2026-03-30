39c52de3f6d6c37b52e476cc7e8796a9a86d23cd
Manas Gogoi <154653360+Skar-X-Manas@users.noreply.github.com>
2026-03-30 15:34:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+data_dir = 'data/'
+db_faiss_path = 'vectorstore/db_faiss'
+
+# Create vector database
+def create_vector_db():
+    loader = DirectoryLoader(data_dir,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
+                                                   chunk_overlap=50)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'})
+
+    db = FAISS.from_documents(texts, embeddings)
+    db.save_local(db_faiss_path)
+
+if __name__ == "__main__":
+    create_vector_db()
